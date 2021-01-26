63b56b109f5723f5e004edd5130f752accb5ceea
John <glennan20@gmail.com>
2021-01-26 10:15:05 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is John Glennan"
+    end
+
+    get '/hometown' do 
+        "My hometown is TWP"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is Any Song on Hybrid Theory"
+    end
+
 end
